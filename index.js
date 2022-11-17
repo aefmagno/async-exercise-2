dc4f6733ddaf96ba39308a7315afd0b98a0c2383
Aaron Magno <aefmagno@gmaik.com>
2022-11-17 21:10:35 +0800

    included implementation for bonus exercise

--- index.js
+++ index.js
@@ -17,8 +17,81 @@ async function asyncPromise(par1) {
   })
 }
 
+async function asyncParallel1(par1) {
+  return new Promise((resolve, reject) => {
+    if (par1 < 0 || par1 > 50000) {
+      reject(new RangeError('Par1 is Invalid'))
+    } else if (typeof par1 === 'string') {
+      reject(new TypeError('Par1 is Not a Number'))
+    }
+
+    setTimeout(() => {
+      if (par1 / 2 > 10000) {
+        reject(new Error('Parallel #1 Timeout'))
+      } else {
+        resolve(true)
+      }
+    }, par1 / 2)
+  })
+}
+
+async function asyncParallel2(par1) {
+  return new Promise((resolve, reject) => {
+    if (par1 < 0 || par1 > 50000) {
+      reject(new RangeError('Par1 is Invalid'))
+    } else if (typeof par1 === 'string') {
+      reject(new TypeError('Par1 is Not a Number'))
+    }
+
+    setTimeout(() => {
+      if (par1 / 3 > 10000) {
+        reject(new Error('Parallel #2 Timeout'))
+      } else {
+        resolve(true)
+      }
+    }, par1 / 3)
+  })
+}
+
+async function asyncParallel3(par1) {
+  return new Promise((resolve, reject) => {
+    if (par1 < 0 || par1 > 50000) {
+      reject(new RangeError('Par1 is Invalid'))
+    } else if (typeof par1 === 'string') {
+      reject(new TypeError('Par1 is Not a Number'))
+    }
+
+    setTimeout(() => {
+      if (par1 / 4 > 10000) {
+        reject(new Error('Parallel #3 Timeout'))
+      } else {
+        resolve(true)
+      }
+    }, par1 / 4)
+  })
+}
+
+async function runAsyncParallels(par1) {
+  const promise1 = asyncParallel1(par1)
+  const promise2 = asyncParallel2(par1)
+  const promise3 = asyncParallel3(par1)
+
+  Promise.allSettled([promise1, promise2, promise3])
+    .then((result) => {
+      console.log('all parallel calls completed')
+      result.forEach((element) => {
+        if (element.reason !== undefined) {
+          console.log(element.reason.toString())
+        } else {
+          console.log(element.value.toString())
+        }
+      })
+    })
+}
+
 async function asyncCB(par1, cb) {
-  await asyncPromise(par1)
+  runAsyncParallels(par1)
+  asyncPromise(par1)
     .then((result) => {
       cb(null, result)
       asyncCB(result, cb)
